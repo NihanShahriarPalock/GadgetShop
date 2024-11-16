2ab0ed7622edab55fd1a3fbeac96c29ee66d6951
NihanShahriarPalock <nihanshahriar000@gmail.com>
2024-11-16 13:30:00 +0600

    Banner

--- Client/src/components/home/Banner.jsx
+++ Client/src/components/home/Banner.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <>
+      <div
+        className='hero min-h-screen'
+        style={{
+          backgroundImage:
+            "url(https://img.pikbest.com/wp/202408/tablet-monitor-office-essentials-technology-and-gadgets-illustration-featuring-laptop-printer-camera-pc-mobile-phone-headphones_9762044.jpg!sw800)",
+        }}>
+        <div className='hero-overlay bg-opacity-60'></div>
+        <div className='hero-content text-neutral-content text-center'>
+          <div className='max-w-md'>
+            <h1 className='mb-5 text-5xl font-bold'>Welcome to Gadget Shop</h1>
+            <p className='mb-5'>
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className='btn btn-primary'>Get Started</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Banner
+export default Banner;
