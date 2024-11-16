c23c99ec47c478b9c6b33bd42dee5df4cb05a814
NihanShahriarPalock <nihanshahriar000@gmail.com>
2024-11-16 15:54:43 +0600

    Login Register Static page

--- Client/src/routes/routes.jsx
+++ Client/src/routes/routes.jsx
@@ -1,6 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import Home from "../pages/Home";
+import About from "../pages/About";
+import Products from "../pages/Products";
+import Contact from "../pages/Contact";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 export const router = createBrowserRouter([
   {
@@ -9,10 +14,28 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element:<Home></Home>
+        element: <Home></Home>,
       },
-      
-    ]
-     
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/products",
+        element: <Products></Products>,
+      },
+      {
+        path: "/contact-us",
+        element: <Contact></Contact>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
